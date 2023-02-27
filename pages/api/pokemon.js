02ba946234215d4b2dfa6e24fc97610f6f8990cf
itc-n22025 <n22025@std.it-college.co.jp>
2023-02-27 00:32:16 +0000

    javascriptAPI

---
+++ pages/api/pokemon.js
@@ -0,0 +1,6 @@
+export default async function handler (req, res) {
+  const { id } = req.query
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const pokemon = await response.json()
+  res.status(200).json(pokemon)
+}
